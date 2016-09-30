eb3a83da65c47a628b7ba237671c427650a8cf4f
Anthony Bloomer <ant0@protonmail.ch>
2016-09-30 22:51:10 +0100

    Update

--- daftlistings/__init__.py
+++ daftlistings/__init__.py
@@ -3,28 +3,30 @@ import urllib
 
 
 class Daft:
+    base = 'http://www.daft.ie'
+
+    sale_types = {
+        'houses': '/houses-for-sale/',
+        'properties': '/property-for-sale/',
+        'auction': '/houses-for-auction/',
+        'apartments': '/apartments-for-sale/'
+    }
+
+    rent_types = {
+        'houses': '/houses-for-rent/',
+        'apartments': '/apartments-for-rent/'
+    }
+
+    query_params = {
+        'sale_agreed': '&s[area_type]=on&s[agreed]=1&s[advanced]=1',
+        'sale_agreed_price': '&s%5Bagreed%5D=1&s%5Badvanced%5D=1',
+        'min_price': '&s%5Bmnp%5D=',
+        'max_price': '&s%5Bmxp%5D=',
+        'ignore_agents': '&s%5Bignored_agents%5D%5B1%5D'
+    }
+
     def __init__(self):
-        self.base = 'http://www.daft.ie'
-
-        self.sale_types = {
-            'houses': '/houses-for-sale/',
-            'properties': '/property-for-sale/',
-            'auction': '/houses-for-auction/',
-            'apartments': '/apartments-for-sale/'
-        }
-
-        self.rent_types = {
-            'houses': '/houses-for-rent/',
-            'apartments': '/apartments-for-rent/'
-        }
-
-        self.query_params = {
-            'sale_agreed': '&s[area_type]=on&s[agreed]=1&s[advanced]=1',
-            'sale_agreed_price': '&s%5Bagreed%5D=1&s%5Badvanced%5D=1',
-            'min_price': '&s%5Bmnp%5D=',
-            'max_price': '&s%5Bmxp%5D=',
-            'ignore_agents': '&s%5Bignored_agents%5D%5B1%5D'
-        }
+        pass
 
     def get_listings(self, county, area=None, offset=0, min_price=None, max_price=None, listing_type='properties',
                      sale_agreed=False, sale_type='sale'):
