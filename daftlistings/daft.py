0328032261e422780aac0340f47db35e748141a6
Anthony Bloomer <ant0@protonmail.ch>
2017-06-11 19:59:01 +0100

    Update

--- daftlistings/daft.py
+++ daftlistings/daft.py
@@ -7,21 +7,20 @@ from exception import DaftException
 
 class Daft(object):
     def __init__(self):
-        self.verbose = False
-        self.area = None
-        self.offset = 0
-        self.county = None
-        self.min_price = None
-        self.max_price = None
-        self.min_beds = None
-        self.max_beds = None
-        self.listing_type = SaleType.PROPERTIES
-        self.sale_agreed = False
-        self.min_beds = None
-        self.max_beds = None
-        self.sort_by = None
-        self.sort_order = None
-        self.commercial_property_type = None
+        self._base = 'https://www.daft.ie/'
+        self._verbose = False
+        self._area = None
+        self._offset = 0
+        self._county = None
+        self._min_price = None
+        self._max_price = None
+        self._min_beds = None
+        self._max_beds = None
+        self._listing_type = SaleType.PROPERTIES
+        self._sale_agreed = False
+        self._sort_by = None
+        self._sort_order = None
+        self._commercial_property_type = None
         self._query_params = ""
         self._price = ""
 
@@ -30,7 +29,7 @@ class Daft(object):
         Set to True to print the HTTP status code and HTML content when making a request to Daft.
         :param verbose
         """
-        self.verbose = verbose
+        self._verbose = verbose
 
     def set_area(self, area):
         """
@@ -38,7 +37,7 @@ class Daft(object):
         :param area:
         :return:
         """
-        self.area = area.replace(" ", "-").lower()
+        self._area = area.replace(" ", "-").lower()
 
     def set_county(self, county):
         """
@@ -46,7 +45,7 @@ class Daft(object):
         :param county:
         :return:
         """
-        self.county = county.replace(" ", "-").lower()
+        self._county = county.replace(" ", "-").lower()
 
     def set_offset(self, offset):
         """
@@ -54,7 +53,13 @@ class Daft(object):
         :param offset:
         :return:
         """
-        self.offset = offset
+
+        try:
+            int(offset)
+        except:
+            raise Exception("Offset should be an integer.")
+
+        self._offset = str(offset)
 
     def set_min_price(self, min_price):
         """
@@ -67,8 +72,8 @@ class Daft(object):
         except:
             raise Exception("Min price should be an integer.")
 
-        self.min_price = str(min_price)
-        self._price += str(QueryParam.MIN_PRICE) + self.min_price
+        self._min_price = str(min_price)
+        self._price += str(QueryParam.MIN_PRICE) + self._min_price
 
     def set_max_price(self, max_price):
         """
@@ -81,8 +86,8 @@ class Daft(object):
         except:
             raise Exception("Min price should be an integer.")
 
-        self.max_price = str(max_price)
-        self._price += str(QueryParam.MAX_PRICE) + self.max_price
+        self._max_price = str(max_price)
+        self._price += str(QueryParam.MAX_PRICE) + self._max_price
 
     def set_listing_type(self, listing_type):
         """
@@ -92,7 +97,7 @@ class Daft(object):
         :param listing_type:
         :return:
         """
-        self.listing_type = listing_type
+        self._listing_type = listing_type
 
     def set_sale_agreed(self, sale_agreed):
         """
@@ -100,7 +105,7 @@ class Daft(object):
         :param sale_agreed:
         :return:
         """
-        self.sale_agreed = sale_agreed
+        self._sale_agreed = sale_agreed
 
     def set_min_beds(self, min_beds):
         """
@@ -109,14 +114,14 @@ class Daft(object):
         :return:
         """
 
-        self.min_beds = str(min_beds)
+        self._min_beds = str(min_beds)
 
         try:
             int(min_beds)
         except:
             raise Exception("Minimum number of beds should be an integer.")
 
-        self._query_params += str(QueryParam.MIN_BEDS) + self.min_beds
+        self._query_params += str(QueryParam.MIN_BEDS) + self._min_beds
 
     def set_max_beds(self, max_beds):
         """
@@ -129,8 +134,8 @@ class Daft(object):
         except:
             raise Exception("Maximum number of beds should be an integer.")
 
-        self.max_beds = str(max_beds)
-        self._query_params += str(QueryParam.MAX_BEDS) + self.max_beds
+        self._max_beds = str(max_beds)
+        self._query_params += str(QueryParam.MAX_BEDS) + self._max_beds
 
     def set_sort_by(self, sort_by):
         """
@@ -138,7 +143,7 @@ class Daft(object):
         :param sort_by:
         :return:
         """
-        self.sort_by = str(sort_by)
+        self._sort_by = str(sort_by)
 
     def set_sort_order(self, sort_order):
         """
@@ -146,7 +151,7 @@ class Daft(object):
         :param sort_order:
         :return:
         """
-        self.sort_order = str(sort_order)
+        self._sort_order = str(sort_order)
 
     def set_commercial_property_type(self, commercial_property_type):
         """
@@ -154,7 +159,7 @@ class Daft(object):
         :param commercial_property_type:
         :return:
         """
-        self.commercial_property_type = str(commercial_property_type)
+        self._commercial_property_type = str(commercial_property_type)
 
     def get_listings(self):
         """
@@ -162,35 +167,33 @@ class Daft(object):
         :return: Listing object
         """
 
-        if self.area is None:
-            self.area = ''
+        if self._area is None:
+            self._area = ''
 
-        if self.sale_agreed:
-            if self.min_price or self.max_price:
+        if self._sale_agreed:
+            if self._min_price or self._max_price:
                 self._query_params += self._price + str(QueryParam.SALE_AGREED_WITH_PRICE)
             else:
                 self._query_params += str(QueryParam.SALE_AGREED)
         else:
-            if self.min_price or self.max_price:
+            if self._min_price or self._max_price:
                 self._query_params += self._price
 
-        if self.min_price or self.max_price and isinstance(self.listing_type, RentType):
+        if self._min_price or self._max_price and isinstance(self._listing_type, RentType):
             self._query_params += str(QueryParam.IGNORED_AGENTS)
 
-        if self.sort_by:
-            if self.sort_order:
-                self._query_params += str(QueryParam.SORT_ORDER) + str(self.sort_order)
-                self._query_params += str(QueryParam.SORT_BY) + str(self.sort_by)
+        if self._sort_by:
+            if self._sort_order:
+                self._query_params += str(QueryParam.SORT_ORDER) + str(self._sort_order)
+                self._query_params += str(QueryParam.SORT_BY) + str(self._sort_by)
             else:
                 self._query_params += str(QueryParam.SORT_ORDER) + str(SortOrder.DESCENDING)
-                self._query_params += str(QueryParam.SORT_BY) + self.sort_by
+                self._query_params += str(QueryParam.SORT_BY) + self._sort_by
 
-        commercial = str(self.commercial_property_type) if self.commercial_property_type is not None else ''
-        query = 'http://www.daft.ie/' + self.county + str(self.listing_type) + str(self.area) + commercial \
-                + '?offset=' + str(self.offset) + self._query_params
-
-        req = requests.get(query)
-        if self.verbose:
+        commercial = self._commercial_property_type if self._commercial_property_type is not None else ''
+        req = requests.get(self._base + self._county + str(self._listing_type) + str(self._area) + commercial
+                           + '?offset=' + str(self._offset) + self._query_params)
+        if self._verbose:
             print("Status code: " + str(req.status_code))
             print("HTML: " + req.content)
         if req.status_code != 200:
--- daftlistings/listing.py
+++ daftlistings/listing.py
@@ -8,18 +8,30 @@ class Listing(object):
         self.data = data
 
     def get_price(self):
+        """
+        This method returns the price.
+        :return:
+        """
         try:
             return self.data.find('strong', {'class': 'price'}).text
         except:
             return
 
     def get_price_change(self):
+        """
+        This method returns any price change.
+        :return:
+        """
         try:
             return self.data.find('div', {'class': 'price-changes-sr'}).text
         except:
             return
 
     def get_formalised_address(self):
+        """
+        This method returns the formalised address.
+        :return:
+        """
         try:
             t = self.data.find('a').contents[0]
             s = t.split('-')
@@ -33,6 +45,10 @@ class Listing(object):
             return
 
     def get_address_line_1(self):
+        """
+        This method returns the first line of the address.
+        :return:
+        """
         formalised_address = self.get_formalised_address()
         if formalised_address is None:
             return
@@ -43,6 +59,10 @@ class Listing(object):
             return
 
     def get_address_line_2(self):
+        """
+        This method returns the second line of the address.
+        :return:
+        """
         formalised_address = self.get_formalised_address()
         if formalised_address is None:
             return
@@ -57,6 +77,10 @@ class Listing(object):
             return
 
     def get_town(self):
+        """
+        This method returns the town name.
+        :return:
+        """
         formalised_address = self.get_formalised_address()
         if formalised_address is None:
             return
@@ -67,6 +91,10 @@ class Listing(object):
             return
 
     def get_county(self):
+        """
+        This method returns the county name.
+        :return:
+        """
         formalised_address = self.get_formalised_address()
         if formalised_address is None:
             return
@@ -77,6 +105,10 @@ class Listing(object):
             return
 
     def get_listing_image(self):
+        """
+        This method returns the listing image.
+        :return:
+        """
         try:
             link = self.get_daft_link()
             req = requests.get(link)
@@ -89,6 +121,10 @@ class Listing(object):
             return
 
     def get_agent(self):
+        """
+        This method returns the agent name.
+        :return:
+        """
         try:
             agent = self.data.find('ul', {'class': 'links'}).text
             return agent.split(':')[1].strip()
@@ -96,6 +132,10 @@ class Listing(object):
             return
 
     def get_agent_url(self):
+        """
+        This method returns the agent's url.
+        :return:
+        """
         try:
             agent = self.data.find('ul', {'class': 'links'})
             links = agent.find_all('a')
@@ -104,6 +144,10 @@ class Listing(object):
             return
 
     def get_daft_link(self):
+        """
+        This method returns the url of the listing.
+        :return:
+        """
         link = self.data.find('a', href=True)
         try:
             return 'http://www.daft.ie/' + link['href']
@@ -111,6 +155,10 @@ class Listing(object):
             return
 
     def get_dwelling_type(self):
+        """
+        This method returns the dwelling type.
+        :return:
+        """
         try:
             info = self.data.find('ul', {"class": "info"}).text
             s = info.split('|')
@@ -119,6 +167,10 @@ class Listing(object):
             return
 
     def get_posted_since(self):
+        """
+        This method returns the date the listing was entered.
+        :return:
+        """
         try:
             info = self.data.find('div', {"class": "date_entered"}).text
             s = info.split(':')
@@ -127,6 +179,10 @@ class Listing(object):
             return
 
     def get_num_bedrooms(self):
+        """
+        This method gets the number of bedrooms.
+        :return:
+        """
         try:
             info = self.data.find('ul', {"class": "info"}).text
             s = info.split('|')
@@ -136,6 +192,10 @@ class Listing(object):
             return
 
     def get_num_bathrooms(self):
+        """
+        This method gets the number of bathrooms.
+        :return:
+        """
         try:
             info = self.data.find('ul', {"class": "info"}).text
             s = info.split('|')
@@ -145,6 +205,10 @@ class Listing(object):
             return
 
     def get_area_size(self):
+        """
+        This method returns the area size. This method should only be called when retrieving commercial type listings.
+        :return:
+        """
         try:
             info = self.data.find('ul', {"class": "info"}).text
             s = info.split('|')
