9bc25a50a2f8048f56f89723badd9a004d5b7cbd
Anthony Bloomer <ant0@protonmail.ch>
2017-06-19 12:29:47 +0100

    Update contact_advertiser.py

--- examples/contact_advertiser.py
+++ examples/contact_advertiser.py
@@ -10,9 +10,12 @@ daft.set_max_price(1500)
 listings = daft.get_listings()
 
 for listing in listings:
-    listing.contact_advertiser(
+    contact = listing.contact_advertiser(
         name="Jane Doe",
         contact_number="019202222",
         email="jane@example.com",
         message="Hi, I seen your listing on daft.ie and I would like to schedule a viewing."
     )
+    
+    if contact:
+        print("Advertiser contacted")
