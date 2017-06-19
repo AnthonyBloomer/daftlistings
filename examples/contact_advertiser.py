ca8212a16a22c05d7c1ae9929a15878a990149d0
Anthony Bloomer <ant0@protonmail.ch>
2017-06-19 11:36:40 +0100

    contact advertiser

---
+++ examples/contact_advertiser.py
@@ -0,0 +1,18 @@
+from daftlistings import Daft, RentType
+
+daft = Daft()
+
+daft.set_county("Dublin City")
+daft.set_listing_type(RentType.APARTMENTS)
+daft.set_min_price(1000)
+daft.set_max_price(1500)
+
+listings = daft.get_listings()
+
+for listing in listings:
+    listing.contact_advertiser(
+        name="Jane Doe",
+        contact_number="019202222",
+        email="jane@example.com",
+        message="Hi, I seen your listing on daft.ie and I would like to schedule a viewing."
+    )
