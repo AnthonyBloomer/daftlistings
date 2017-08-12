c83092ccf9060fc4a40d29a1b0db8ba9ae03e668
Anthony Bloomer <ant0@protonmail.ch>
2017-08-12 14:59:22 +0100

    added example

--- daftlistings/request.py
+++ daftlistings/request.py
@@ -18,17 +18,20 @@ class Request:
 
         if req.status_code != 200:
             raise DaftRequestException(status_code=req.status_code, reason=req.reason)
-            
+
         soup = BeautifulSoup(req.content, 'html.parser')
         return soup
 
     def post(self, url, params):
         req = requests.post(url, params=params, headers=self._headers)
-        
+
         print("URL: " + req.url)
         print("Status code: " + str(req.status_code))
-        
+
+        if self._verbose:
+            print req.content
+
         if req.status_code != 200:
             raise DaftRequestException(status_code=req.status_code, reason=req.reason)
-            
+
         return True
