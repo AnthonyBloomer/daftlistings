c32ec3aab16b17034db11dbf4157812dfc91e9f5
Anthony Bloomer <ant0@protonmail.ch>
2018-09-30 19:22:26 +0100

    Update

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class PublishCommand(Command):
 
 
 setup(name='daftlistings',
-      version='1.5.0',
+      version='1.6.0',
       description='A library that enables programmatic interaction with daft.ie. ',
       long_description=long_descr,
       url='https://github.com/AnthonyBloomer/daftlistings',
