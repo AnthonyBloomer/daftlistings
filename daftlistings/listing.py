df97021ee28d3e8b0604fd348cd4613ade2ff7e2
Thiago MadPin <madpin@gmail.com>
2018-10-15 17:26:15 -0300

    Fix error from contact_number to str, not binary

--- daftlistings/listing.py
+++ daftlistings/listing.py
@@ -310,7 +310,7 @@ class Listing(object):
             self._ad_page_content = Request(debug=self._debug).get(self.daft_link)
         try:
             number = self._ad_page_content.find('button', {'class': 'phone-number'})
-            return str(base64.b64decode(number.attrs['data-p']))
+            return (base64.b64decode(number.attrs['data-p'])).decode('ascii')
         except Exception as e:
             if self._debug:
                 self._logger.error("Error getting contact_number. Error message: " + e.message)
