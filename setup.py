f6c1b52425bc740f443b5efe6632a4bf18ee997f
Anthony Bloomer <ant0@protonmail.ch>
2019-04-23 13:05:51 +0100

    Update version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class PublishCommand(Command):
 
 
 setup(name='daftlistings',
-      version='1.7.0',
+      version='1.7.1',
       description='A library that enables programmatic interaction with daft.ie. Daft.ie has nationwide coverage and contains about 80% of the total available properties in Ireland.',
       long_description=long_descr,
       url='https://github.com/AnthonyBloomer/daftlistings',
