747c1b559e5cc96e0e3ed3edeed1c7615255bbf3
Anthony Bloomer <ant0@protonmail.ch>
2020-01-12 19:32:30 +0000

    Update request.py

--- daftlistings/request.py
+++ daftlistings/request.py
@@ -1,5 +1,3 @@
-import logging
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,18 +5,13 @@ from .exceptions import DaftException
 
 
 class Request:
-    def __init__(self, debug=False):
+    def __init__(self):
         self._headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0"
         }
-        self._debug = debug
 
     def get(self, url, params=None):
         req = requests.get(url, headers=self._headers, params=params)
-        if self._debug:
-            logging.info("URL: " + req.url)
-            logging.info("Status code: " + str(req.status_code))
-
         if req.status_code != 200:
             raise DaftException(reason=req.reason)
 
@@ -27,11 +20,6 @@ class Request:
 
     def post(self, url, params=None):
         req = requests.post(url, params=params, headers=self._headers)
-
-        if self._debug:
-            logging.info("URL: " + req.url)
-            logging.info("Status code: " + str(req.status_code))
-
         if req.status_code != 200:
             raise DaftException(reason=req.reason)
 
