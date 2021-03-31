805cf1a249b29dcd9410546907ac53c31c3a432e
Anthony Bloomer <anthony.bloomer@icloud.com>
2021-03-31 22:39:44 +0100

    Fix method

--- examples/properties_for_sale.py
+++ examples/properties_for_sale.py
@@ -5,7 +5,7 @@ daft.set_location(Location.DUBLIN)
 daft.set_search_type(SearchType.RESIDENTIAL_SALE)
 daft.set_property_type(PropertyType.HOUSE)
 daft.set_min_price(400000)
-daft.set_min_price(500000)
+daft.set_max_price(500000)
 
 listings = daft.search()
 
