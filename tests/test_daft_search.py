a1b4aa36f93bb3543db4661675415452c3535b19
Stiofáin <stiofainf@gmail.com>
2021-04-03 16:47:05 +0100

    (#95) Combine tests

--- tests/test_daft_search.py
+++ tests/test_daft_search.py
@@ -42,3 +42,33 @@ class DaftTest(unittest.TestCase):
         daft.search()
 
         mock_post.assert_called_with(url, headers=headers, json=payload)
+
+    def test_any_to_rent(self):
+        daft = Daft()
+        daft.set_search_type(SearchType.RESIDENTIAL_RENT)
+        daft.set_location(Location.DUBLIN)
+        listings = daft.search(max_pages=1)
+        self.assertTrue(len(listings) > 0)
+
+    def test_apartments_to_rent(self):
+        daft = Daft()
+        daft.set_search_type(SearchType.RESIDENTIAL_RENT)
+        daft.set_property_type(PropertyType.APARTMENT)
+        daft.set_location(Location.DUBLIN)
+        listings = daft.search(max_pages=1)
+        self.assertTrue(len(listings) > 0)
+
+    def test_studios_to_rent(self):
+        daft = Daft()
+        daft.set_search_type(SearchType.RESIDENTIAL_RENT)
+        daft.set_property_type(PropertyType.STUDIO_APARTMENT)
+        daft.set_location(Location.DUBLIN)
+        listings = daft.search(max_pages=1)
+        self.assertTrue(len(listings) > 0)
+
+    def test_houses_to_rent(self):
+        daft = Daft()
+        daft.set_search_type(SearchType.RESIDENTIAL_RENT)
+        daft.set_property_type(PropertyType.HOUSE)
+        listings = daft.search(max_pages=1)
+        self.assertTrue(len(listings) > 0)
