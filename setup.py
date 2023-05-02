5c5e44dd6c5893a6901b4f52a9b6c1adca89f418
Anthony Bloomer <anthony.bloomer@icloud.com>
2023-05-02 08:00:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ with open("README.md", "rb") as f:
 class PublishCommand(Command):
     """Support setup.py publish."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -31,43 +31,42 @@ class PublishCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
 
-setup(name='daftlistings',
-      version='2.0.3',
-      description='A library that enables programmatic interaction with daft.ie. Daft.ie has nationwide coverage and contains about 80% of the total available properties in Ireland.',
-      long_description=long_descr,
-      long_description_content_type='text/markdown',
-      url='https://github.com/AnthonyBloomer/daftlistings',
-      author='Anthony Bloomer',
-      keywords=['daft', 'real estate', 'daft.ie'],
-      author_email='ant0@protonmail.ch',
-      license='MIT',
-      packages=['daftlistings'],
-      install_requires=[
-          'requests',
-          'folium'
-      ],
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          "Topic :: Software Development :: Libraries",
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3.9',
-      ],
-      cmdclass={
-          'publish': PublishCommand,
-      },
-      zip_safe=False)
+setup(
+    name="daftlistings",
+    version="2.0.3",
+    description="A library that enables programmatic interaction with daft.ie. Daft.ie has nationwide coverage and contains about 80% of the total available properties in Ireland.",
+    long_description=long_descr,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnthonyBloomer/daftlistings",
+    author="Anthony Bloomer",
+    keywords=["daft", "real estate", "daft.ie"],
+    author_email="ant0@protonmail.ch",
+    license="MIT",
+    packages=["daftlistings"],
+    install_requires=["requests", "folium"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+    ],
+    cmdclass={
+        "publish": PublishCommand,
+    },
+    zip_safe=False,
+)
